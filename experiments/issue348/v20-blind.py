aa568a35d388254cbde81aa245c5499d28b7ee44
Dominik Drexler <drexlerd@informatik.uni-freiburg.de>
2021-01-18 18:37:38 +0100

    changed code to work with newest fd lab version

--- experiments/issue348/v20-blind.py
+++ experiments/issue348/v20-blind.py
@@ -1,66 +1,47 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import itertools
-import os
-
+#from lab.environments import LocalEnvironment
 from lab.environments import FreiburgSlurmEnvironment
 
-from downward.reports.compare import ComparativeReport
+import os.path
+import os
 
-import common_setup
-from common_setup import IssueConfig, IssueExperiment
-from relativescatter import RelativeScatterPlotReport
+from downward.experiment import FastDownwardExperiment
+from downward.reports.absolute import AbsoluteReport
+from downward.reports.scatter import ScatterPlotReport
 
-DIR = os.path.dirname(os.path.abspath(__file__))
-SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
-#BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
-BENCHMARKS_DIR = os.path.expanduser('~/benchmarks/downward-benchmarks')
-REVISIONS = ["issue348-base", "issue348-v20"]
-CONFIGS = [
-    IssueConfig("blind", ["--search", "astar(blind())"]),
-]
 
-ADL_DOMAINS = [
-    "assembly",
-    "miconic-fulladl",
-    "openstacks",
-    "openstacks-opt08-adl",
-    "optical-telegraphs",
-    "philosophers",
-    "psr-large",
-    "psr-middle",
-    "trucks",
-]
-SUITE = common_setup.DEFAULT_OPTIMAL_SUITE + ADL_DOMAINS
-#ENVIRONMENT = BaseSlurmEnvironment(
-#    partition="infai_2",
-#    email="jendrik.seipp@unibas.ch",
-#    export=["PATH", "DOWNWARD_BENCHMARKS"])
-ENVIRONMENT = FreiburgSlurmEnvironment()
+# my version of fast downward
+REPO = os.path.expanduser('~/downward')
+# the set of benchmarks to evaluate on
+BENCHMARKS = os.path.expanduser('~/benchmarks/downward-benchmarks')
+## ALL
+SUITE = ["assembly", "miconic-fulladl", "openstacks", "openstacks-opt08-adl", "optical-telegraphs", "philosophers", "psr-large", "psr-middle", "trucks"]
 
-if common_setup.is_test_run():
-    SUITE = IssueExperiment.DEFAULT_TEST_SUITE + ["openstacks-opt08-adl:p01.pddl"]
-    ENVIRONMENT = LocalEnvironment(processes=1)
+REVISION_CACHE = os.path.expanduser('~/fd-lab-freiburg/experiments/cpu-benchmarks/data/revision-cache')
 
-exp = IssueExperiment(
-    revisions=REVISIONS,
-    configs=CONFIGS,
-    environment=ENVIRONMENT,
-)
-exp.add_suite(BENCHMARKS_DIR, SUITE)
+# Note: if we use 4G=4096M>4000M then 2 cores are being occupied.
+ENV = FreiburgSlurmEnvironment()
 
+# Setup the experiments
+exp = FastDownwardExperiment(environment=ENV, revision_cache=REVISION_CACHE)
+
+exp.add_suite(BENCHMARKS, SUITE)
+
+exp.add_algorithm("issue348-base", REPO, "issue348-base", ["--search", "astar(blind())"])
+exp.add_algorithm("issue348-v20", REPO, "issue348-v20", ["--search", "astar(blind())"])
+
+# "For single-search experiments, we recommend adding the following parsers in this order:"
 exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
 exp.add_parser(exp.SINGLE_SEARCH_PARSER)
 exp.add_parser(exp.PLANNER_PARSER)
 
+# the steps to be executed
 exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
+exp.add_parse_again_step()
 exp.add_fetcher(name='fetch')
-
-#exp.add_absolute_report_step()
-exp.add_comparison_table_step()
-exp.add_scatter_plot_step(relative=True, attributes=["total_time"], suffix="-strips", filter_domain=common_setup.DEFAULT_OPTIMAL_SUITE)
-exp.add_scatter_plot_step(relative=True, attributes=["total_time"], suffix="-adl", filter_domain=ADL_DOMAINS)
+exp.add_report(AbsoluteReport(), outfile='report.html')
 
 exp.run_steps()
